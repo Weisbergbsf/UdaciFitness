59b103f3fa4ab6d45586e3eb3a7b539b7652db71
Weisberg Barros <weisbergbsf@gmail.com>
2019-01-24 21:12:39 -0300

    Created Tab navigator

--- App.js
+++ App.js
@@ -1,13 +1,101 @@
 import React from 'react'
-import { View } from 'react-native'
-import AddEntry from './components/AddEntry';
+import { View, Platform, StatusBar } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import { 
+  createBottomTabNavigator, 
+  createMaterialTopTabNavigator, 
+  createAppContainer, 
+  createStackNavigator } from 'react-navigation'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants  } from 'expo'
+import { purple, white } from './utils/colors'
+import History from './components/History'
+import AddEntry from './components/AddEntry'
+import EntryDetail from './components/EntryDetail'
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+const RouteConfigs = {
+  History: {
+    screen: History,
+    navigationOptions: {
+    tabBarLabel: "History",
+    tabBarIcon: ({ tintColor }) => (
+      <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    )}
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+    tabBarLabel: "Add Entry",
+    tabBarIcon: ({ tintColor }) => (
+      <FontAwesome name='plus-square' size={30} color={tintColor} />
+    )}
+  }
+};
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === "ios" ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === "ios" ? white : purple,
+      shadowColor: "rgba(0, 0, 0, 0.24)",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+};
+
+const Tabs = Platform.OS === "ios"
+  ? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+  : createMaterialTopTabNavigator(RouteConfigs, TabNavigatorConfig);
+
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: ({ navigation }) => ({
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      },
+    }),
+  },
+});
+
+const TabsContainer = createAppContainer(MainNavigator)
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry />
-      </View>
-    );
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content'/>
+          <TabsContainer />
+        </View>
+      </Provider>
+    )
   }
 }
